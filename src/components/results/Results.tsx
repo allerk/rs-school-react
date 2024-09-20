2a13cefebad002e5a9047973cf01c39aaf734032
Aleksandr Lerko <allerk@ttu.ee>
2024-09-20 18:36:52 +0300

    feat: update restructure logic on Results.tsx and rename it

--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,36 +1,39 @@
 import { Component, ReactNode } from 'react';
-import { IApiResponse } from '../../interfaces/IApiResponse.ts';
-import './Result.css';
+import { CharacterInfo } from '../../interfaces/IApiResponse.ts';
+import './Results.css';
 
 interface IProps {
-  results: IApiResponse[];
-  isLoading: boolean;
+  results: CharacterInfo[];
+  isStart: boolean;
+  noResults: boolean;
 }
 
-export class Result extends Component<IProps> {
+export class Results extends Component<IProps> {
   render(): ReactNode {
+    const { results, isStart, noResults } = this.props;
+    if (isStart) {
+      return <p>Try to find something!</p>;
+    }
+
     return (
-      <>
-        <div>
-          {this.props.results.length > 0 && !this.props.isLoading ? (
-            <ul>
-              {this.props.results.map((item) => (
-                <li key={item.name}>
-                  The character - <span className="bold-text">{item.name}</span>{' '}
-                  is <span className="red-text">{item.height}</span> height and
-                  it weighs around <span className="red-text">{item.mass}</span>{' '}
-                  kgs. <span className="bold-text">{item.name}</span> identifies
-                  as a <span className="red-text">{item.gender}</span> person
-                  and has a <span className="red-text">{item.eye_color}</span>{' '}
-                  eyes.
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
-      </>
+      <div>
+        {!noResults ? (
+          <ul>
+            {results.map((item) => (
+              <li key={item.name}>
+                The character - <span className="bold-text">{item.name}</span>{' '}
+                is <span className="red-text">{item.height}</span> height and it
+                weighs around <span className="red-text">{item.mass}</span> kgs.{' '}
+                <span className="bold-text">{item.name}</span> identifies as a{' '}
+                <span className="red-text">{item.gender}</span> person and has a{' '}
+                <span className="red-text">{item.eye_color}</span> eyes.
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Nothing was found. Try again!</p>
+        )}
+      </div>
     );
   }
 }
